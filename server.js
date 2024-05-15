06c92d6737121def01b1ab9f325debf5918b6bf5
LucasGitHubx <lucasdarosa450@gmail.com>
2024-05-15 06:35:26 -0300

    Changing routes

--- server.js
+++ server.js
@@ -5,52 +5,37 @@ const { get, getOne, post, put, remove } = require("./db/methods");
 
 const app = express();
 const corsOptions = {
-  origin: "https://lucasgithubx.github.io/express_crud_mongodb.com",
+  origin: "*",
 };
 
 app.use(express.json());
 app.use(cors(corsOptions));
 connect();
 
-app.get(
-  "https://lucasgithubx.github.io/express_crud_mongodb//posts",
-  (req, res) => {
-    get(res);
-  }
-);
+app.get("/posts", (req, res) => {
+  get(res);
+});
 
-app.get(
-  "https://lucasgithubx.github.io/express_crud_mongodb//posts/:id",
-  (req, res) => {
-    const id = req.params.id;
-    getOne(res, id);
-  }
-);
+app.get("/posts/:id", (req, res) => {
+  const id = req.params.id;
+  getOne(res, id);
+});
 
-app.post(
-  "https://lucasgithubx.github.io/express_crud_mongodb//posts",
-  (req, res) => {
-    const body = req.body;
-    post(res, body);
-  }
-);
+app.post("/posts", (req, res) => {
+  const body = req.body;
+  post(res, body);
+});
 
-app.put(
-  "https://lucasgithubx.github.io/express_crud_mongodb//posts/:id",
-  (req, res) => {
-    const body = req.body;
-    const id = req.params.id;
-    put(res, id, body);
-  }
-);
+app.put("/posts/:id", (req, res) => {
+  const body = req.body;
+  const id = req.params.id;
+  put(res, id, body);
+});
 
-app.delete(
-  "https://lucasgithubx.github.io/express_crud_mongodb//posts/:id",
-  (req, res) => {
-    const id = req.params.id;
-    remove(res, id);
-  }
-);
+app.delete("/posts/:id", (req, res) => {
+  const id = req.params.id;
+  remove(res, id);
+});
 
 app.listen(3000);
 console.log("server has been established on port 3000");
