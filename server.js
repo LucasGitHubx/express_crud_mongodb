854751c377a34cb91aa09e02c5a39d336b9a44cf
LucasGitHubx <lucasdarosa450@gmail.com>
2024-05-15 06:30:47 -0300

    Changing routes

--- server.js
+++ server.js
@@ -12,30 +12,45 @@ app.use(express.json());
 app.use(cors(corsOptions));
 connect();
 
-app.get("/posts", (req, res) => {
-  get(res);
-});
+app.get(
+  "https://lucasgithubx.github.io/express_crud_mongodb//posts",
+  (req, res) => {
+    get(res);
+  }
+);
 
-app.get("/posts/:id", (req, res) => {
-  const id = req.params.id;
-  getOne(res, id);
-});
+app.get(
+  "https://lucasgithubx.github.io/express_crud_mongodb//posts/:id",
+  (req, res) => {
+    const id = req.params.id;
+    getOne(res, id);
+  }
+);
 
-app.post("/posts", (req, res) => {
-  const body = req.body;
-  post(res, body);
-});
+app.post(
+  "https://lucasgithubx.github.io/express_crud_mongodb//posts",
+  (req, res) => {
+    const body = req.body;
+    post(res, body);
+  }
+);
 
-app.put("/posts/:id", (req, res) => {
-  const body = req.body;
-  const id = req.params.id;
-  put(res, id, body);
-});
+app.put(
+  "https://lucasgithubx.github.io/express_crud_mongodb//posts/:id",
+  (req, res) => {
+    const body = req.body;
+    const id = req.params.id;
+    put(res, id, body);
+  }
+);
 
-app.delete("/posts/:id", (req, res) => {
-  const id = req.params.id;
-  remove(res, id);
-});
+app.delete(
+  "https://lucasgithubx.github.io/express_crud_mongodb//posts/:id",
+  (req, res) => {
+    const id = req.params.id;
+    remove(res, id);
+  }
+);
 
 app.listen(3000);
 console.log("server has been established on port 3000");
